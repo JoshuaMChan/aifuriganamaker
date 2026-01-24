1fcae0889fef2e41e0fe57a110160ef22e90523f
joshuachen <lancemchan@sina.com>
2026-01-24 17:20:20 +0800

    Change UI

--- extension/src/entrypoints/options/components/Footer.tsx
+++ extension/src/entrypoints/options/components/Footer.tsx
@@ -1,9 +1,4 @@
-import { Trans, useTranslation } from "react-i18next";
-
-const generateYear = () => {
-  const year = new Date().getFullYear();
-  return year === 2023 ? year : `2023-${year}`;
-};
+import { useTranslation } from "react-i18next";
 
 interface FooterLinkProps {
   href: string;
@@ -30,36 +25,11 @@ export function Footer() {
     <footer className="border-gray-200 border-t dark:border-slate-800">
       <div className="mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8">
         <p className="flex flex-col flex-wrap gap-1.5 py-8 text-center text-sm sm:flex-row">
-          <span className="block whitespace-nowrap sm:inline">
-            <Trans
-              i18nKey="footerCopyright"
-              values={{ yearsRange: generateYear() }}
-              components={{
-                author: <FooterLink content="aiktb" href="https://github.com/aiktb" />,
-              }}
-            />
-          </span>
           <span className="block sm:inline">
-            <Trans
-              i18nKey="footerLicense"
-              components={{
-                license: (
-                  <FooterLink
-                    content="MIT"
-                    href="https://github.com/aiktb/furiganamaker/blob/main/LICENSE"
-                  />
-                ),
-              }}
-            />
+            Thank you so much to <FooterLink content="aiktb" href="https://github.com/aiktb" /> for
+            creating the original Furigana&nbsp;Maker and sharing it with the community.
           </span>
-          <span className="block sm:inline">
-            <Trans
-              i18nKey="footerBuiltWith"
-              components={{
-                tool: <FooterLink content="WXT" href="https://wxt.dev" />,
-              }}
-            />
-          </span>
+          <span className="block sm:inline"> This AI agent fork is built on that work.</span>
         </p>
         <a
           className="flex size-9 items-center justify-center rounded-md text-black hover:bg-gray-100 dark:text-white dark:hover:bg-slate-800 dark:hover:text-white"
