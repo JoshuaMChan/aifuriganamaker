47c6a2727072138432447f30979886ee94e9897b
joshuachen <lancemchan@sina.com>
2026-02-02 14:03:06 +0800

    more prompt optimization

--- extension/src/agent/prompt.ts
+++ extension/src/agent/prompt.ts
@@ -2,7 +2,7 @@ export function audit(input: string): string {
   return `
     ## Task
     The cvs below the text are the furigana of the original kanjis. Those numbers are indices in the string.
-    If any furigana is wrong, correct it. Compute the excat array of indices of corrections.
+    If any furigana is wrong, correct it. Compute the exact array of indices of corrections.
 
     ## Input Data
     ${input}
